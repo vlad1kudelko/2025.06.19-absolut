dd6f494f817ef3506b3ae96e0d4f7b0c312be940
Vlad Kudelko <vlad1kudelko@gmail.com>
2025-06-23 21:10:43 +0800

    -

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,14 +22,6 @@ const Home = () => {
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: 6 }}>
-      <Box textAlign="center">
-        <Typography variant="h3" fontWeight="bold" gutterBottom>
-          Добро пожаловать в Absolut
-        </Typography>
-        <Typography variant="h6" color="text.secondary" maxWidth={600} mx="auto">
-          Здесь в будущем будет отображаться статистика по базе с фильтром.
-        </Typography>
-      </Box>
       <Box>
         <Typography variant="h5" fontWeight="bold" gutterBottom>
           Отчёт по доставкам за последние 30 дней
