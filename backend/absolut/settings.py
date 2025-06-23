8641b8d3f0a86e04e272804859ae0ba16655be3b
Vlad Kudelko <vlad1kudelko@gmail.com>
2025-06-23 23:53:46 +0800

    -

--- backend/absolut/settings.py
+++ backend/absolut/settings.py
@@ -100,7 +100,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+STATIC_URL = '/djstatic/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
